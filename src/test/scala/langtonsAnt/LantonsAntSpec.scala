752c4cda30a3dc32c94f35ff460ff4a04069b3a5
juan <juanan_rp@hotmail.co.uk>
2018-06-21 13:14:05 +0100

    first test placeholder

--- src/test/scala/langtonsAnt/LantonsAntSpec.scala
+++ src/test/scala/langtonsAnt/LantonsAntSpec.scala
@@ -3,6 +3,8 @@ package langtonsAnt
 import org.scalatest._
 
 class LantonsAntSpec extends FlatSpec with Matchers {
+
+  //What's the simplest test possible we can do?
   "First test here" should
     "write my first test here" in {
       Hello.greeting shouldEqual "hello"
