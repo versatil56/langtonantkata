f1dcddc8b9ce5c9ff639b03ab8ed47b13d7fcf99
juan <juanan_rp@hotmail.co.uk>
2018-06-29 14:24:48 +0100

    fixing test, moving on

--- src/test/scala/langtonsAnt/LantonsAntSpec.scala
+++ src/test/scala/langtonsAnt/LantonsAntSpec.scala
@@ -4,11 +4,18 @@ import org.scalatest._
 
 class LantonsAntSpec extends FlatSpec with Matchers {
 
-  "An ant" should {
+  "An ant" should
     "be defined in the application" in {
       val ant = new Ant()
       ant.isInstanceOf[Ant] shouldBe true
     }
-  }
 
+  it should "be aware of its current position" in {
+    val ant = new Ant()
+    ant.currentPosition shouldBe (0,0)
+  }
+}
+
+case class Ant(){
+  def currentPosition : (Int,Int) = ???
 }
