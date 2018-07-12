8ba3b207ef378593b8de0d1fb35735434ff2befa
Eulogio Gutierrez <eulogio.gutierrez@digital.hmrc.gov.uk>
2018-07-12 15:10:51 +0100

    fixed current position, testing for facing direction

--- src/test/scala/langtonsAnt/LantonsAntSpec.scala
+++ src/test/scala/langtonsAnt/LantonsAntSpec.scala
@@ -14,8 +14,14 @@ class LantonsAntSpec extends FlatSpec with Matchers {
     val ant = new Ant()
     ant.currentPosition shouldBe (0,0)
   }
+
+  it should "be aware of its facing direction" in {
+    val ant = new Ant()
+    ant.facingDirection shouldBe 0 // East
+  }
 }
 
-case class Ant(){
-  def currentPosition : (Int,Int) = ???
+case class Ant(x: Int = 0, y: Int = 0){
+  def currentPosition : (Int,Int) = (x, y)
+  def facingDirection : Int = ???
 }
